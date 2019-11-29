437d64ee13a6bf948b5350b14aa7f32ce3f4b896
vonaxl <vonaxlcorso@gmail.com>
2019-11-29 10:32:43 +0100

    changed AjaxCall for tv and movies, cleaned the code ans some minor changes

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,6 +1,6 @@
-function callMovie(titolo) {
+function callAjax(url, titolo, type) { //Ajax set up from SEARCH-FUNTION
   $.ajax({
-    url: "https://api.themoviedb.org/4/search/movie",
+    url: url,
     method: "GET",
     data: {
       api_key: "0878a4f792be7a8e086e7aa5c1356f41",
@@ -9,60 +9,33 @@ function callMovie(titolo) {
     },
     success: function(dati) {
       console.log(dati.results);
-      var movies = dati.results;
-      print('movie',movies);
-      if (movies.length>0){
+      var elements = dati.results;
+      print(type, elements);
+      if (elements.length > 0) {
         inputReset();
       }
-
-      //   per ogni object dentro l'array dati.response pusha il template clonato dentro il container #albums
-      // dati.results.forEach(z => {
-      //   console.log(z);
-      //   var sorgente = $("#hb-template").html();
-      //   var sorgDigerita = Handlebars.compile(sorgente);
-      //   var objRef = {img:z.backdrop_path, title:z.title ,titleOrg:z.original_title,voto:z.vote_average };
-      //   var elValorizzato  = sorgDigerita(objRef);
-      //   $("#films").append(elValorizzato);
-
-      // });
     },
     error: function() {}
   }); //fine $.ajax
 }
-function callTv(titolo) {
-  $.ajax({
-    url: "https://api.themoviedb.org/4/search/tv",
-    method: "GET",
-    data: {
-      api_key: "0878a4f792be7a8e086e7aa5c1356f41",
-      language: "it-IT",
-      query: titolo
-    },
-    success: function(dati) {
-      var movies = dati.results;
-      print('tv',movies);
-    },
-    error: function() {}
-  }); //fine $.ajax
+function inputReset() { //reset type input 
+  $("#cerca").val("");
 }
-function inputReset(){
-  $('#cerca').val('');
-}
-function print(type, movies) {
+function print(type, elements) {//  OUTPUT
   $("#cerca").html("");
-  movies.forEach(z => {
+  var sorgente = $("#movies").html();
+  var sorgDigerita = Handlebars.compile(sorgente);
+  elements.forEach(z => { //cicle to every array object
     // console.log(z);
-    var sorgente = $("#movies").html();
-    var sorgDigerita = Handlebars.compile(sorgente);
     // save the vote to be used on a function
     var title, orig_title, printOuput;
-    if (type =="movie") {
+    if (type == "movie") {
       title = z.title;
       orig_title = z.original_title;
       printOuput = $("#films");
-    }else {
+    } else {
       title = z.name;
-      orig_title= z.original_name;
+      orig_title = z.original_name;
       printOuput = $("#tvSeries");
     }
     var vote = z.vote_average;
@@ -73,36 +46,37 @@ function print(type, movies) {
       titleOrg: orig_title,
       lingua: z.original_language,
       nazione: nation(flag),
-      voto: z.vote_average+' '+stars(vote),
+      voto: z.vote_average + " " + stars(vote),
       trama: z.overview
     };
-    
+
     var elValorizzato = sorgDigerita(objRef);
     printOuput.append(elValorizzato);
   });
 }
 
-function stars(vote){
+function stars(vote) {//star counters
   var vote5 = Math.floor(vote / 2);
-  var vuoto5= 5 - vote5 ;
+  var vuoto5 = 5 - vote5;
   console.log(vote5);
   var starFont = "";
   for (let i = 0; i < 5; i++) {
-  //   if (i <= vote5) {
-  //     starFont += '<i class="fas fa-star"> </i>';
-  //   }else{
-  //     starFont += '<i class="far fa-star"></i>';
-  //   }
-  // OPERATORE TERNARNIO
-    starFont += (i <= vote5) 
-      ? '<i class="fas fa-star"> </i>'
-      : '<i class="far fa-star"></i>';
+    //   if (i <= vote5) {
+    //     starFont += '<i class="fas fa-star"> </i>';
+    //   }else{
+    //     starFont += '<i class="far fa-star"></i>';
+    //   }
+    // OPERATORE TERNARNIO
+    starFont +=
+      i <= vote5
+        ? '<i class="fas fa-star"> </i>'
+        : '<i class="far fa-star"></i>';
   }
   return starFont;
 }
 
-function nation(flag){
-  var paese = ['it','cn','en','ja','fr','hi'];
+function nation(flag) { //display flags by language 
+  var paese = ["it", "cn", "en", "ja", "fr", "hi"];
   // if (flag === "it") {
   //   paese = 'it.png';
   // }else if (flag === "cn") {
@@ -118,39 +92,38 @@ function nation(flag){
   // }else {
   //   paese = 'cn.png';
   // }
-  if (paese.includes(flag)){
-    return flag+'.png';
+  if (paese.includes(flag)) {
+    return flag + ".png";
   }
-  return '';
+  return "";
+}
+function reset() {//reset #films and #tvSeries HTML
+  
+  $("#films").html("");
+  $("#tvSeries").html("");
+}
+function search() { //get the element title lowercase it and replace spaces reset html and set up the link
+  
+  var titolo = $("#cerca")
+    .val()
+    .toLowerCase()
+    .replace(/ /g, "+");
+  reset();
+
+  var urlMovie = "https://api.themoviedb.org/3/search/movie";
+  var urlTv = "https://api.themoviedb.org/3/search/tv";
+  callAjax(urlMovie, titolo, "movie");
+  callAjax(urlTv, titolo, "tv");
 }
 
 $(document).ready(function() {
-  // .toLowerCase().replace(/ /g,"+");
-  $("#invia").click(function() {
-    // $('#films,#tvSeries').css('he')
-    var titolo = $("#cerca")
-      .val()
-      .toLowerCase()
-      .replace(/ /g, "+");
-    $("#films").html("");
-    $("#tvSeries").html("");
-    console.log(titolo);
-    callMovie(titolo);
-    callTv(titolo);
-  });
-  $(this).on('mouseenter mouseleave', '.cardBody', function(e){
-    var pict = $(this).css('background-image')
+  $("#invia").click(search);
+
+  $(this).on("mouseenter mouseleave", ".cardBody", function(e) {
+    var pict = $(this).css("background-image");
     pict = pict.replace(/(?:^url\(["']?|["']?\)$)/g, "");
-    console.log(pict);
-    
-    var bg = e.type ==='mouseenter' ? pict : ' ';
-    
-    $('.wrapper').css('background-image','url('+bg+')');
-    
-  });
-  // $(this).on('mouseleave', '.cardBody', function(){
-  //   $(this).find('.box').css('display', 'none')
-  //   console.log("enter");
-  // });
+    var bg = e.type === "mouseenter" ? pict : " ";
+    $("#backBg").css("background-image", "url(" + bg + ")");
+  });//fine function mouse leave e mouse enter
+
 });
- 
